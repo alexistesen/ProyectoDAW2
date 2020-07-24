30598e391ffa4eedf2d60481882e222ad4cd0f10
Luis Bustamante <ljbustamante@gmail.com>
2020-07-23 21:11:14 -0500

    Fix: update provincia and distrito lists poblate

--- src/main/java/com/cibertec/controller/PersonaController.java
+++ src/main/java/com/cibertec/controller/PersonaController.java
@@ -32,86 +32,81 @@ public class PersonaController {
 
 	@Autowired
 	private IDepartamentoService dptoService;
-	
+
 	@Autowired
 	private IProvinciaService provinciaService;
-	
+
 	@Autowired
 	private IDistritoService distritoService;
-	
-	@GetMapping({"/","/login"})
+
+	@GetMapping({ "/", "/login" })
 	public String index() {
 		return "index";
 	}
-	
+
 	@RequestMapping("/inicio")
 	public String index(Model model) {
-		model.addAttribute("list",personaService.getAll());
+		model.addAttribute("list", personaService.getAll());
 		return "inicio";
 	}
-	
+
 	@GetMapping("/save/{id}")
 	public String showSave(@PathVariable("id") Integer id, Model model) {
-		model.addAttribute("listaDepartamento",dptoService.findAll());
-		if(id != null && id != 0) {
+		model.addAttribute("listaDepartamento", dptoService.findAll());
+		if (id != null && id != 0) {
 			model.addAttribute("persona", personaService.get(id));
 			model.addAttribute("selectedOn", "true");
-			
-			/*1.*/
-			List<LcldProvincia> listaTotal = provinciaService.findAll();
+
+			List<LcldProvincia> listaProvincias = provinciaService.findAll();
 			List<LcldProvincia> lista = new ArrayList<LcldProvincia>();
-			
-			if(personaService.get(id).getProvincia().getProvincia_id()!=0) {
-			for(LcldProvincia temp: listaTotal) {
-				if(temp.getDepartamento().getDpto_id()==personaService.get(id).getProvincia().getProvincia_id()) {
-					lista.add(temp);
+
+			if (personaService.get(id).getProvincia().getProvincia_id() != 0) {
+				System.out.println("Existe provincia");
+				for (LcldProvincia temp : listaProvincias) {
+					if (temp.getDepartamento().getDpto_id() == personaService.get(id).getDepartamento().getDpto_id()) {
+						lista.add(temp);
+					}
 				}
 			}
-			}
-			
-			model.addAttribute("listaProvincia",lista);
-			/**/
-			
-			/*2.*/
-			
-			List<LcldDistrito> listaTotal2 = distritoService.findAll();
+
+			model.addAttribute("listaProvincia", lista);
+
+			List<LcldDistrito> listaDistritos = distritoService.findAll();
 			List<LcldDistrito> lista2 = new ArrayList<LcldDistrito>();
-			
-			if(personaService.get(id).getDistrito().getDistrito_id()!=0) {
-			for(LcldDistrito temp: listaTotal2) {
-				if(temp.getProvincia().getProvincia_id()==personaService.get(id).getDistrito().getDistrito_id()) {
-					lista2.add(temp);
+
+			if (personaService.get(id).getDistrito().getDistrito_id() != 0) {
+				System.out.println("Existe distrito");
+				for (LcldDistrito temp : listaDistritos) {
+					if (temp.getProvincia().getProvincia_id() == personaService.get(id).getProvincia()
+							.getProvincia_id()) {
+						lista2.add(temp);
+					}
 				}
 			}
-			}
-		
-			model.addAttribute("listaDistrito",lista2);
-			
-			
-			/**/
-			
-			
-			
+
+			model.addAttribute("listaDistrito", lista2);
+
 		} else {
 			model.addAttribute("persona", new Persona());
 		}
 		return "save";
 	}
-	
+
 	@PostMapping("/save")
-	public String save(@Valid @ModelAttribute Persona persona, BindingResult result, Model model, RedirectAttributes attribute) {
+	public String save(@Valid @ModelAttribute Persona persona, BindingResult result, Model model,
+			RedirectAttributes attribute) {
 		if (result.hasErrors()) {
-			model.addAttribute("listaDepartamento",dptoService.findAll());
-			System.out.println("Existieron errores en el formulario");			
+			model.addAttribute("listaDepartamento", dptoService.findAll());
+			System.out.println("Existieron errores en el formulario");
 			return "save";
 		}
-		
+
 		personaService.save(persona);
 		System.out.println("Cliente guardado con exito!");
 		attribute.addFlashAttribute("success", "Cliente guardado con éxito!");
 		return "redirect:/inicio";
 	}
-	
+
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id, Model model, RedirectAttributes attribute) {
 		personaService.delete(id);
@@ -119,43 +114,41 @@ public class PersonaController {
 		attribute.addFlashAttribute("warning", "Registro Eliminado con Éxito!");
 		return "redirect:/inicio";
 	}
-	
 
-	@PostMapping({"/save/listaProvincia","/listaProvincia"})
+	@PostMapping({ "/save/listaProvincia", "/listaProvincia" })
 	public String provincia(Model model, @Valid Integer departamento) {
-		
+
 		List<LcldProvincia> listaTotal = provinciaService.findAll();
 		List<LcldProvincia> lista = new ArrayList<LcldProvincia>();
-		
-		if(departamento!=0) {
-		for(LcldProvincia temp: listaTotal) {
-			if(temp.getDepartamento().getDpto_id()==departamento) {
-				lista.add(temp);
+
+		if (departamento != 0) {
+			for (LcldProvincia temp : listaTotal) {
+				if (temp.getDepartamento().getDpto_id() == departamento) {
+					lista.add(temp);
+				}
 			}
 		}
-		}
-		
-		model.addAttribute("listaProvincia",lista);
+
+		model.addAttribute("listaProvincia", lista);
 		return "cboProvincia";
 	}
-		
-	
-	@PostMapping({"/save/listaDistrito","/listaDistrito"})
+
+	@PostMapping({ "/save/listaDistrito", "/listaDistrito" })
 	public String distrito(Model model, @Valid Integer provincia) {
-		
+
 		List<LcldDistrito> listaTotal = distritoService.findAll();
 		List<LcldDistrito> lista = new ArrayList<LcldDistrito>();
-		
-		if(provincia!=0) {
-		for(LcldDistrito temp: listaTotal) {
-			if(temp.getProvincia().getProvincia_id()==provincia) {
-				lista.add(temp);
+
+		if (provincia != 0) {
+			for (LcldDistrito temp : listaTotal) {
+				if (temp.getProvincia().getProvincia_id() == provincia) {
+					lista.add(temp);
+				}
 			}
 		}
-		}
-	
-		model.addAttribute("listaDistrito",lista);
+
+		model.addAttribute("listaDistrito", lista);
 		return "cboDistrito";
 	}
-	
+
 }
